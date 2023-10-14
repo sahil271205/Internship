d5f397acd364e0c25e043f1dd6636e867013273b
sahil271205 <93194585+sahil271205@users.noreply.github.com>
2023-10-14 21:04:54 +0530

    Add files via upload

---
+++ sahil rane_4SF21CY036/day3/index.js
@@ -0,0 +1,57 @@
+//prompt("Enter your name","")
+//console.log("this is console");
+//console.log(6+5);
+//document.write("<h1>javascript </h1>");
+
+//var num1=5;
+//var num1=6
+//num1=8
+//console.log(num1)
+
+//let num2=9
+//let num2=10
+//num2=11
+//console.log(num2);
+
+//let num3=10
+//let num3=11
+//num3=15
+//console.log(num3);
+
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1==num2);
+
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+// }
+// document.write(mul(4,5,6,7))
+// function greet(){
+//     alert("hello everyone");
+
+//}
+// let age=10;
+
+// if(age>=18){
+//     document.write("eligible for vote");
+// }else{
+
+//     document.write("not eligible");
+// }
+// for(let num=10;num>=1;num--){
+//     console.log(num);
+// }
+//  dom - document object model
+//  Dom Manipulation Methods
+//   document.getElementById()
+// document.getElementsByClassName() 
+//  document.getElementsByTagName() 
+ 
+//  document.querySelector() 
+//   document.querySelectorAll()
+
+//let el=document.getElementById("head").innerhtml;
+//console.log(el0;
+let ele=document.querySelector("#head").innerHTML;
+console.log(ele)
